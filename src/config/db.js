7641e9bdcf6bfcb843153e05b7db16782d8aa073
Abisha Mohanathas <abishamohanathas0431@gmail.com>
2025-12-30 10:49:25 +0530

    time changed

--- src/config/db.js
+++ src/config/db.js
@@ -11,7 +11,7 @@ const connectDB = async () => {
 
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
-      serverSelectionTimeoutMS: 5000, // Timeout after 5s instead of 30s
+      serverSelectionTimeoutMS: 50000, // Timeout after 50s instead of 30s
       socketTimeoutMS: 45000, // Close sockets after 45s of inactivity
     });
 
